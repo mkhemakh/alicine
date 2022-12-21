3d9c10afd041696e4d2fd508e654d24d46a6e09a
mkhemakh <wizgha.rottila28@gmail.com>
2022-12-21 17:03:08 +0100

    third form

--- src/components/pages/filmForm.js
+++ src/components/pages/filmForm.js
@@ -1,48 +1,58 @@
-import {useState} from "react";
-import {addFilm} from "../actions/filmActions";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {addFilm, setNewFilm} from "../actions/filmActions";
 
-const FilmForm = () => {
-
-    const [film, setFilm] = useState([]);
-    const [nom, setNom] = useState('');
-    const [date, setDate] = useState('');
-    const [categorie, setCategorie] = useState('');
-
+const filmForm = () => {
     const dispatch = useDispatch();
+    const { acteurs, errorMsgs, newFilm } = useSelector( state => state.filmReducer );
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        setFilm({
-            id: new Date().getTime(),
-            nom,
-            date,
-            
-        });
-        setNom('');
-        dispatch( addDragon( { dragon } ) )
+        e.preventDefault();
+        dispatch( addFilm() );
     }
 
     return (
         <form onSubmit={ (e) => handleSubmit(e) }>
-            <h1>New Film</h1>
+            <h2>Nouveau Film</h2>
+                { errorMsgs.length > 0 && (
+                    <ul className="error-box">
+                        { errorMsgs.map( (error, i) => <li key={i}>{ error }</li> ) }
+                    </ul>
+                )}
             <p>
-                <label htmlFor="nom">Nom</label>
-                <input name="nom" type="text" value={nom} onChange={ (e) => setNom( e.target.value ) }/>
+                <label htmlFor="titre">Name</label>
+                <input titre="titre" type="text"
+                       value={newFilm.titre}
+                       onChange={ (e) => dispatch( setNewFilm( { newFilm: {
+                               ...newFilm,
+                               titre: e.target.value,
+                       }}))}
+                />
             </p>
             <p>
-                <label htmlFor="age">Age</label>
-                <input name="age" type="text" value={age} onChange={ (e) => setAge( e.target.value ) }/>
+                <label htmlFor="duree">Age</label>
+                <input titre="duree" type="number"
+                       value={newFilm.duree}
+                       onChange={ (e) => dispatch( setNewFilm( { newFilm: {
+                               ...newFilm,
+                               duree: e.target.value,
+                       }}))}
+                />
             </p>
             <p>
-                <label htmlFor="element">Element</label>
-                <input name="element" type="tel" value={element} onChange={ (e) => setElement( e.target.value ) }/>
+                <label htmlFor="type">Element</label>
+                <select titre="type"
+                        value={newFilm.type}
+                        onChange={ (e) => dispatch( setNewFilm( { newFilm: {
+                                ...newFilm,
+                                type: e.target.value,
+                        }}))}
+                >
+                    { types.map( (type, i) => <option key={i} value={type}>{type}</option> ) }
+                </select>
             </p>
             <p>
-                <button type="submit">Add</button>
+                <button type="submit">Ajouter film</button>
             </p>
         </form>
     )
 }
-
-export default FilmForm;
