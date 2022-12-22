645d445a2e77a285881866be6bc2aab0093e0f4c
mkhemakh <wizgha.rottila28@gmail.com>
2022-12-22 14:17:58 +0100

    signup2

--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -1,19 +1,26 @@
-const signup = () => {
+import "../../Styles/signup.css";
+function signup() {
+  return (
+    <div className="signup">
+      <h1>SIGNUP</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Email </label>
+          <input name="email" type="mail" />
+        </div>
+        <div>
+          <label htmlFor="username">Username </label>
+          <input name="username" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">Password </label>
+          <input name="password" type="password" />
+        </div>
 
-return (
-    <>
-      <label>SIGNUP</label>
-      <div>
-        <form>
-          <label htmlFor="email">Email: </label>
-          <input name="email" type="mail"/>
-          <label htmlFor="username">Username: </label>
-          <input name="username" type="text"/>
-          <label htmlFor="password">Password: </label>
-          <input name="password" type="password"/>
-          <button type="submit">git </button>
-        </form>
-      </div>
-    </>
-    )
+        <button type="submit">S'inscrire</button>
+      </form>
+    </div>
+  );
 }
+
+export default signup;
