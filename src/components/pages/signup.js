aea7ec23723dc6bcbc010f47e6dc16cc551793c3
mkhemakh <wizgha.rottila28@gmail.com>
2022-12-22 10:11:19 +0100

    signup

--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -1,1 +1,19 @@
-const signup
+const signup = () => {
+
+return (
+    <>
+      <label>SIGNUP</label>
+      <div>
+        <form>
+          <label htmlFor="email">Email: </label>
+          <input name="email" type="mail"/>
+          <label htmlFor="username">Username: </label>
+          <input name="username" type="text"/>
+          <label htmlFor="password">Password: </label>
+          <input name="password" type="password"/>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </>
+    )
+}
